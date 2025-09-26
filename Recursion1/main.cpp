7c41d3b80a55cb4aba626d91cc0040039fc5b836
Nikita <Nikita@DESKTOP-13IEN5T>
2025-09-26 19:39:09 +0500

    factorial done

---
+++ Recursion1/main.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+
+void elevator(int floor);
+int Factorial(int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите номер этажа: "; cin >> n;
+	elevator(n);
+	//cout << "Recusion";
+	//main();
+}
+void elevator(int floor)
+{
+	if (floor == 0)
+	{
+		cout << "Вы в подвале" << endl;
+		return;
+	}
+	cout << "Вы на " << floor << " этаже" << endl;
+	elevator(floor - 1);
+	cout << "Вы на " << floor << " этаже" << endl;
+}
+
+int Factorial(int n)
+{
+	if (n == 0)
+		return 1;
+	else
+		return n * Factorial(n - 1);
+}
